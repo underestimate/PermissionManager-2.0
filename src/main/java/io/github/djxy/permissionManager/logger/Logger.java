8b34fbfec552e00fd8b2b798e4ee407d831fb9e4
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-08-20 20:49:19 -0400

    Logger created

---
+++ src/main/java/io/github/djxy/permissionManager/logger/Logger.java
@@ -0,0 +1,60 @@
+package io.github.djxy.permissionManager.logger;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * Created by Samuel on 2016-08-17.
+ */
+public class Logger {
+
+    private static LoggerMode loggerMode = LoggerMode.NO_LOG;
+
+    public static void setLoggerMode(LoggerMode mode){
+        Preconditions.checkNotNull(mode);
+
+        loggerMode = mode;
+    }
+
+    private final Class clazz;
+
+    public Logger(Class clazz) {
+        this.clazz = clazz;
+    }
+
+    public void info(String message){
+        if(loggerMode == LoggerMode.NO_LOG)
+            return;
+        else if(loggerMode == LoggerMode.DEBUG_SERVER)
+            org.slf4j.LoggerFactory.getLogger(clazz).info(message);
+        else if(loggerMode == LoggerMode.DEBUG_IDE) {
+            synchronized (message) {
+                System.out.println("[" + System.currentTimeMillis() + "][" + clazz.getSimpleName() + "][INFO]: " + message);
+            }
+        }
+    }
+
+    public void warn(String message){
+        if(loggerMode == LoggerMode.NO_LOG)
+            return;
+        else if(loggerMode == LoggerMode.DEBUG_SERVER)
+            org.slf4j.LoggerFactory.getLogger(clazz).warn(message);
+        else if(loggerMode == LoggerMode.DEBUG_IDE){
+            synchronized (message) {
+                System.out.println("["+System.currentTimeMillis()+"]["+clazz.getSimpleName()+"][WARN]: "+message);
+            }
+        }
+    }
+
+    public void error(String message){
+        if(loggerMode == LoggerMode.NO_LOG)
+            return;
+        else if(loggerMode == LoggerMode.DEBUG_SERVER)
+            org.slf4j.LoggerFactory.getLogger(clazz).error(message);
+        else if(loggerMode == LoggerMode.DEBUG_IDE){
+            synchronized (message) {
+                System.err.println("["+System.currentTimeMillis()+"]["+clazz.getSimpleName()+"][ERROR]: "+message);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/io/github/djxy/permissionManager/logger/LoggerMode.java
@@ -0,0 +1,10 @@
+package io.github.djxy.permissionManager.logger;
+
+/**
+ * Created by Samuel on 2016-08-17.
+ */
+public enum LoggerMode {
+    NO_LOG,
+    DEBUG_SERVER,
+    DEBUG_IDE
+}
