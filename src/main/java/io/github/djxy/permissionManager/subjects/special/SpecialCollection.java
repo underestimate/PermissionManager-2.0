f6cefdca9ec816d45ee8b2e5b7fedbba397baa94
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-08-20 20:51:40 -0400

    The special subject.

---
+++ src/main/java/io/github/djxy/permissionManager/subjects/special/SpecialCollection.java
@@ -0,0 +1,89 @@
+package io.github.djxy.permissionManager.subjects.special;
+
+import io.github.djxy.permissionManager.exceptions.SubjectIdentifierExistException;
+import io.github.djxy.permissionManager.exceptions.SubjectIdentifierInvalidException;
+import io.github.djxy.permissionManager.subjects.SubjectCollection;
+import org.spongepowered.api.service.permission.Subject;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Samuel on 2016-08-17.
+ */
+public class SpecialCollection extends SubjectCollection {
+
+    public SpecialCollection(String identifier) {
+        super(identifier, "Special");
+    }
+
+    @Override
+    public String getIdentifier() {
+        return super.getIdentifier();
+    }
+
+    @Override
+    public Subject get(String s) {
+        return new Special(s, this);
+    }
+
+    @Override
+    public boolean hasRegistered(String s) {
+        return super.hasRegistered(s);
+    }
+
+    @Override
+    public Iterable<Subject> getAllSubjects() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Map<Subject, Boolean> getAllWithPermission(String s) {
+        return new HashMap<>();
+    }
+
+    @Override
+    public Map<Subject, Boolean> getAllWithPermission(Set set, String s) {
+        return new HashMap<>();
+    }
+
+    @Override
+    public Subject getDefaults() {
+        return new Special("", this);
+    }
+
+    @Override
+    public synchronized void save(String identifier) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized void save() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized boolean load(String identifier) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public synchronized void load() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setDirectory(Path directory) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected Subject createSubjectFromFile(String identifier) throws SubjectIdentifierInvalidException, SubjectIdentifierExistException {
+        throw new UnsupportedOperationException();
+    }
+
+}
