3a1acba928fc39ca0565a1277e643bb2891dd83d
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-08-25 11:48:35 -0400

    Create translator object from the files

--- src/main/java/io/github/djxy/permissionManager/util/ResourceUtil.java
+++ src/main/java/io/github/djxy/permissionManager/util/ResourceUtil.java
@@ -3,10 +3,17 @@ package io.github.djxy.permissionmanager.util;
 import com.google.common.base.Preconditions;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
+import io.github.djxy.permissionmanager.language.Language;
 import io.github.djxy.permissionmanager.logger.Logger;
+import io.github.djxy.permissionmanager.translator.Translator;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.ConfigurationOptions;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -47,6 +54,32 @@ public class ResourceUtil {
         }
 
         return new JsonArray();
+    }
+
+    public synchronized static Translator loadTranslations() {
+        Translator translator = new Translator();
+
+        for (Language language : Language.getLanguages()) {
+            try {
+                ConfigurationLoader loader = HoconConfigurationLoader
+                        .builder()
+                        .setDefaultOptions(ConfigurationOptions.defaults())
+                        .setSource(() -> new BufferedReader(new InputStreamReader(object.getClass().getClassLoader().getResource("translations/"+language.getISO639_3()+".hocon").openStream())))
+                        .build();
+                ConfigurationNode node = loader.load();
+
+                Map<Object, ConfigurationNode> map = (Map<Object, ConfigurationNode>) node.getChildrenMap();
+
+                for (Object code : map.keySet())
+                    translator.addTranslation(language, code.toString(), map.get(code).getString(""));
+
+            } catch (Exception e) {
+                LOGGER.error("Couldn't read " + language.getISO639_3()+".hocon");
+                e.printStackTrace();
+            }
+        }
+
+        return translator;
     }
 
 }
