7ecbeb9afd0abc4fb18e220b6d5a8d5882c12e94
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-08-25 11:57:39 -0400

    New exception

---
+++ src/main/java/io/github/djxy/permissionmanager/exceptions/UserCreationException.java
@@ -0,0 +1,11 @@
+package io.github.djxy.permissionmanager.exceptions;
+
+/**
+ * Created by Samuel on 2016-08-23.
+ */
+public class UserCreationException extends RuntimeException {
+
+    public UserCreationException(String message) {
+        super(message);
+    }
+}
