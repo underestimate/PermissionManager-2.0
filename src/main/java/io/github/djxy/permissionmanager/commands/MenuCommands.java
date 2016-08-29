bda87afe5cbc708f5d0f7811775b4f7a079c2c40
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-08-29 16:09:18 -0400

    Commands to use the menu

---
+++ src/main/java/io/github/djxy/permissionmanager/commands/MenuCommands.java
@@ -0,0 +1,42 @@
+package io.github.djxy.permissionmanager.commands;
+
+import io.github.djxy.customcommands.annotations.CustomCommand;
+import io.github.djxy.permissionmanager.menu.menus.MainMenu;
+import io.github.djxy.permissionmanager.translator.Translator;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.entity.living.player.Player;
+
+import java.util.Map;
+
+/**
+ * Created by Samuel on 2016-08-29.
+ */
+public class MenuCommands extends Command {
+
+    private static final String PERMISSION_MENU = "permissionmanager.commands.menu";
+
+    public MenuCommands(Translator translator) {
+        super(translator);
+    }
+
+    @CustomCommand(
+            command = "pm",
+            permission = PERMISSION_MENU,
+            parsers = {}
+    )
+    public void openMenu1(CommandSource source, Map<String, Object> values) {
+        if(source instanceof Player)
+            new MainMenu((Player) source, translator).sendToPlayer();
+    }
+
+    @CustomCommand(
+            command = "pm menu",
+            permission = PERMISSION_MENU,
+            parsers = {}
+    )
+    public void openMenu2(CommandSource source, Map<String, Object> values) {
+        if(source instanceof Player)
+            new MainMenu((Player) source, translator).sendToPlayer();
+    }
+
+}
