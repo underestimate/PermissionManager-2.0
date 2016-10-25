1aadd8451765d1084d16dddbc45a947485f74e8b
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-10-25 14:37:37 -0400

    Fixed the getOption and getPermissionValue

--- src/main/java/io/github/djxy/permissionmanager/subjects/special/Default.java
+++ src/main/java/io/github/djxy/permissionmanager/subjects/special/Default.java
@@ -1,30 +1,33 @@
 package io.github.djxy.permissionmanager.subjects.special;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Sets;
-import io.github.djxy.permissionmanager.subjects.*;
+import io.github.djxy.permissionmanager.logger.Logger;
+import io.github.djxy.permissionmanager.subjects.Permission;
+import io.github.djxy.permissionmanager.subjects.Subject;
+import io.github.djxy.permissionmanager.subjects.SubjectData;
 import io.github.djxy.permissionmanager.subjects.group.Group;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.service.context.Context;
-import org.spongepowered.api.service.permission.*;
-import org.spongepowered.api.service.permission.Subject;
 import org.spongepowered.api.service.permission.SubjectCollection;
-import org.spongepowered.api.service.permission.SubjectData;
 import org.spongepowered.api.util.Tristate;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Created by Samuel on 2016-10-13.
  */
 public class Default implements org.spongepowered.api.service.permission.Subject {
 
+    private static final Logger LOGGER = new Logger(Default.class);
+
     public static final String IDENTIFIER = "Default";
     public static final Default instance = new Default();
 
-    private final SubjectData data = new io.github.djxy.permissionmanager.subjects.SubjectData(this);
-    private final SubjectData transientData = new io.github.djxy.permissionmanager.subjects.SubjectData(this);
+    private final SubjectData data = new SubjectData(this);
+    private final SubjectData transientData = new SubjectData(this);
 
     private Default() {
     }
@@ -55,22 +58,77 @@ public class Default implements org.spongepowered.api.service.permission.Subject
     }
 
     @Override
-    public Tristate getPermissionValue(Set<Context> set, String s) {
+    public Tristate getPermissionValue(Set<Context> set, String permission) {
+        Preconditions.checkNotNull(set);
+        Preconditions.checkNotNull(permission);
+        Permission perm;
+
+        if(data.containsContexts(set) && (perm = data.getContextContainer(set).getPermissions().getPermission(permission)) != null) {
+            Subject.logGetPermissionValue(LOGGER, this, set, permission, Tristate.fromBoolean(perm.getValue()));
+            return Tristate.fromBoolean(perm.getValue());
+        }
+
+        if(data.containsContexts(set) && (perm = transientData.getContextContainer(set).getPermissions().getPermission(permission)) != null) {
+            Subject.logGetPermissionValue(LOGGER, this, set, permission, Tristate.fromBoolean(perm.getValue()));
+            return Tristate.fromBoolean(perm.getValue());
+        }
+
+        Subject.logGetPermissionValue(LOGGER, this, set, permission, Tristate.UNDEFINED);
+
         return Tristate.UNDEFINED;
     }
 
     @Override
-    public boolean isChildOf(Set<Context> set, Subject subject) {
-        return false;
+    public boolean isChildOf(org.spongepowered.api.service.permission.Subject parent) {
+        return isChildOf(getActiveContexts(), parent);
     }
 
     @Override
-    public List<Subject> getParents(Set<Context> set) {
-        return new ArrayList<>();
+    public boolean isChildOf(Set<Context> set, org.spongepowered.api.service.permission.Subject subject) {
+        Preconditions.checkNotNull(set);
+        Preconditions.checkNotNull(subject);
+
+        return subject instanceof Group && (data.getParents(set).contains(subject) || transientData.getParents(set).contains(subject));
     }
 
     @Override
-    public Optional<String> getOption(Set<Context> set, String s) {
+    public Optional<String> getOption(String key) {
+        return getOption(org.spongepowered.api.service.permission.SubjectData.GLOBAL_CONTEXT, key);
+    }
+
+    @Override
+    public List<org.spongepowered.api.service.permission.Subject> getParents() {
+        return getParents(org.spongepowered.api.service.permission.SubjectData.GLOBAL_CONTEXT);
+    }
+
+    @Override
+    public List<org.spongepowered.api.service.permission.Subject> getParents(Set<Context> set) {
+        ArrayList<org.spongepowered.api.service.permission.Subject> groups = new ArrayList<>();
+
+        groups.addAll(getSubjectData().getParents(set));
+        groups.addAll(getTransientSubjectData().getParents(set));
+
+        return groups;
+    }
+
+    @Override
+    public Optional<String> getOption(Set<Context> set, String key) {
+        Preconditions.checkNotNull(set);
+        Preconditions.checkNotNull(key);
+        String option;
+
+        if(data.containsContexts(set) && (option = data.getContextContainer(set).getOption(key)) != null) {
+            Subject.logGetOption(LOGGER, this, set, key, Optional.of(option));
+            return Optional.of(option);
+        }
+
+        if(transientData.containsContexts(set) && (option = transientData.getContextContainer(set).getOption(key)) != null) {
+            Subject.logGetOption(LOGGER, this, set, key, Optional.of(option));
+            return Optional.of(option);
+        }
+
+        Subject.logGetOption(LOGGER, this, set, key, Optional.empty());
+
         return Optional.empty();
     }
 
