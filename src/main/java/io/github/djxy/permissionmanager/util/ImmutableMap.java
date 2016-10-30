1c11819830de3487c0b21ecc9f40ad22542f261b
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-10-30 11:52:33 -0400

    ImmutableMap added

---
+++ src/main/java/io/github/djxy/permissionmanager/util/ImmutableMap.java
@@ -0,0 +1,81 @@
+package io.github.djxy.permissionmanager.util;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Samuel on 2016-10-30.
+ */
+public class ImmutableMap<K,V> implements Map<K,V> {
+
+    private Map<K,V> map;
+
+    public ImmutableMap(Map<K,V> map) {
+        this.map = map;
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return map.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return map.containsValue(value);
+    }
+
+    @Override
+    public V get(Object key) {
+        return map.get(key);
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+    }
+
+    @Override
+    public void clear() {
+        map = com.google.common.collect.ImmutableMap.of();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return map.keySet();
+    }
+
+    @Override
+    public Collection<V> values() {
+        return map.values();
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return map.entrySet();
+    }
+
+    @Override
+    public String toString() {
+        return map.toString();
+    }
+}
