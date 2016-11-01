a511442d3ea0eef82db4ff4bef44d36ccd196b48
Samuel Marchildon-Lavoie <samuel.m.lavoie@gmail.com>
2016-11-01 01:13:24 -0400

    Update version

--- src/main/java/io/github/djxy/permissionmanager/PermissionManager.java
+++ src/main/java/io/github/djxy/permissionmanager/PermissionManager.java
@@ -38,6 +38,7 @@ import java.nio.file.Path;
 /**
  * Created by Samuel on 2016-08-07.
  */
+@Plugin(id = "permissionmanager", name = "PermissionManager", version = "2.5.0", authors = {"Djxy"})
 public class PermissionManager {
 
     private static final int FG_COMPAT_MIN_VERSION = 1;
